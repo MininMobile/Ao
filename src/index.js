c198f1b544e8a7ecb714f144b197cb1673bfb517
MininMobile <notminin@gmail.com>
2018-07-14 14:54:24 +0200

    add compile argument

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const compiler = require("./compiler");
 const fs = require("fs");
 
 const version = "1.0.0";
@@ -10,10 +11,23 @@ if (process.argv[0].includes("node")) {
 switch (process.argv[0]) {
 	case "-h":case "--help":
 		console.log("Ao Compiler");
+		console.log("");
+		console.log("\t-v --version\tget compiler version");
+		console.log("\t-h --help\tget compiler help");
+		console.log("\t-c --compile\tcompiles target, output file can be specified");
 		break;
 
 	case "-v":case "--version":
 		console.log(`v${version}`);
+		break;
+
+	case "-c":case "--compile":
+		fs.readFile(process.argv[1], "utf-8", (err, data) => {
+			if (err) return console.error(err);
+
+			let c = new compiler();
+			c.compile(data);
+		});
 		break;
 
 	default:
